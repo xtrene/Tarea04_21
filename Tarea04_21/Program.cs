02378bcc54c0c6ff4ca793284a5f5aed2b97a292
Carlos <Carlos@xtrene.com>
2021-06-22 13:42:40 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_21/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Tarea04_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("Escribe un día de la semana (en minúscula)");
+                string dia = Console.ReadLine();
+
+                switch (dia)
+                {
+                    case "lunes":
+                    case "martes":
+                    case "miércoles":
+                    case "jueves":
+                    case "viernes":
+                        Console.WriteLine("Día Laboral ...Toca trabajar.");
+                        break;
+                    case "sábado":
+                    case "domingo":
+                        Console.WriteLine("Día no laboral ... Relájate... es FIN DE SEMANA.");
+                        break;
+                    default:
+                        Console.WriteLine("ERROR ese día no existe...");
+                        break;
+                }
+
+
+
+                Console.ReadKey();
+            }
+    }
+}
